8953855a45316798b26a0bafdb327e37d4301922
jjoaolucas <joaolucas-05@outlook.com>
2023-11-06 12:12:55 -0300

    feats: cadastro de usuário e lista em forma de tab

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ function processarCadastroUsuario(requisicao, reposta){
     const usuario = {
         nome: requisicao.query.nome,
         sobrenome: requisicao.query.sobrenome,
+        nomeUsuario: requisicao.query.nomeUsuario,
         cidade: requisicao.query.cidade,
         uf: requisicao.query.uf,
         cep: requisicao.query.cep,
@@ -57,10 +58,7 @@ function processarCadastroUsuario(requisicao, reposta){
                         <a class="btn btn-primary" href="/" role"button"> Voltar ao menu </a>
                         <a class="btn btn-primary" href="/cadastroUsuario.html" role"button"> Continuar cadastrando </a>
                     </body>
-                    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-                    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-                    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-
+                    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script
                     </html>
                 `;
                 reposta.end(conteudoResposta);
@@ -81,10 +79,12 @@ app.get('/', (requisicao, reposta) =>{
             <title>Menu do Sistema</title>
         </head>
         <body>
-            <h1>Menu</>
-            <ul><a href="/cadastroUsuario.html>Cadastrar Usuario </a></ul>
+            <h1>Menu</h1>
+            <ul>
+                <li><a href="/cadastroUsuario.html">Cadastrar Usuario</a></li>
+            </ul>
         </body>
-        `)
+        `);
 })
 
 app.get('/cadastrarUsuario' , processarCadastroUsuario);
